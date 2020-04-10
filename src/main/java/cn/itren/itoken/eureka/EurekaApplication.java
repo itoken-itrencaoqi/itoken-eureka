4910c65b1739659376b073f7d54f6d30b3161b40
itrencaoqi <itren_caoqi@163.com>
2020-04-10 20:57:44 +0800

    添加pom.xml，编写启动类。

---
+++ src/main/java/cn/itren/itoken/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itren.itoken.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: itoken
+ * @description: 注册中心启动类
+ * @author: Mr.Ren
+ * @create: 2020-04-10 20:41
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
